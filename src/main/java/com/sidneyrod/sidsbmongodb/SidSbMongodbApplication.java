82ae2f30b5ec5661933800f5379a3de0408e5032
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-03-03 10:27:18 -0300

    Project created

---
+++ src/main/java/com/sidneyrod/sidsbmongodb/SidSbMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.sidneyrod.sidsbmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidSbMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidSbMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sidneyrod/sidsbmongodb/SidSbMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidneyrod.sidsbmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidSbMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
