c8b18d6ff17a8a5310ca4b923105e46c7bb4021c
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-03-04 15:51:59 -0300

    Database instantiation operation

---
+++ src/main/java/com/sidneyrod/sidsbmongodb/config/TestConfig.java
@@ -0,0 +1,32 @@
+package com.sidneyrod.sidsbmongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.sidneyrod.sidsbmongodb.domain.User;
+import com.sidneyrod.sidsbmongodb.repositories.UserRepository;
+
+@Configuration
+public class TestConfig implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
+		
+		User beatrice = new User(null, "Beatrice Rodrigues", "beatrice@gmail.com");
+		User michael = new User(null, "Michael Rodrigues", "michael@gmail.com");
+		User mark = new User(null, "Mark Rodrigues", "mark@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(beatrice, michael, mark));
+		
+	}
+	
+	
+}
