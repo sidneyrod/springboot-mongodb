6e26b857bc79085e2208f07da4ad96554da6400a
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-03-04 20:41:47 -0300

    Getting a user by id

--- src/main/java/com/sidneyrod/sidsbmongodb/services/UserService.java
+++ src/main/java/com/sidneyrod/sidsbmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.sidneyrod.sidsbmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sidneyrod.sidsbmongodb.domain.User;
 import com.sidneyrod.sidsbmongodb.repositories.UserRepository;
+import com.sidneyrod.sidsbmongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,11 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+		}
+
 	
 }
