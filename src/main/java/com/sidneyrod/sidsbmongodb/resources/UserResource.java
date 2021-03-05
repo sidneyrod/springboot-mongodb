31bd24f6d44bf8b383bc59216e2b0668af455703
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-03-05 15:40:31 -0300

    Endpoint to return a user's posts

--- src/main/java/com/sidneyrod/sidsbmongodb/resources/UserResource.java
+++ src/main/java/com/sidneyrod/sidsbmongodb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.sidneyrod.sidsbmongodb.domain.Post;
 import com.sidneyrod.sidsbmongodb.domain.User;
 import com.sidneyrod.sidsbmongodb.dto.UserDTO;
 import com.sidneyrod.sidsbmongodb.services.UserService;
@@ -60,6 +61,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 }
