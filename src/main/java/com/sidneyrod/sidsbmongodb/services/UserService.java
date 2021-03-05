6d9119fdc5a07e5ac9731ed95e8810a74af2fecf
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-03-05 12:03:23 -0300

    User update with PUT

--- src/main/java/com/sidneyrod/sidsbmongodb/services/UserService.java
+++ src/main/java/com/sidneyrod/sidsbmongodb/services/UserService.java
@@ -35,6 +35,19 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User entity = findById(obj.getId());
+		updateData(entity, obj);
+		return repository.save(entity);
+
+	}
+	
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
